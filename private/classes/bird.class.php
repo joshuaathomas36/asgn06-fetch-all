8dfb2616e2ad76df58e588114409fee8eac5a6ad
Joshua Thomas <joshuaathomas36@students.abtech.edu>
2021-10-14 14:53:55 -0400

    asgn06 complete

--- private/classes/bird.class.php
+++ private/classes/bird.class.php
@@ -17,17 +17,28 @@
         4 => "Extreme concern"
     ];
 
+    
+    /**
+     * The __construct method uses the array given to make a asortment of varibles to display later
+     *
+     * @param   Array  $row  The array that will be used
+     */
     public function __construct($row=[]) {
-        $this->common_name = $row['common_name'] ?? '';
-        $this->habitat = $row['habitat'] ?? '';
-        $this->food = $row['food'] ?? '';
-        $this->nest_palcement = $row['nest_palcement'] ?? '';
-        $this->behavior = $row['behavior'] ?? '';
-        $this->backyard_tips = $row['backyard_tips'] ?? '';
-        $this->conservation_id = $row['conservation_id'] ?? '';
-
+        $this->common_name = htmlentities($row['common_name'] ?? '');
+        $this->habitat = htmlentities($row['habitat'] ?? '');
+        $this->food = htmlentities($row['food'] ?? '');
+        $this->nest_palcement = htmlentities($row['nest_palcement'] ?? '');
+        $this->behavior = htmlentities($row['behavior'] ?? '');
+        $this->backyard_tips = htmlentities($row['backyard_tips'] ?? '');
+        $this->conservation_id = htmlentities($row['conservation_id'] ?? '');
     }
 
+    /**
+     * The conservation method is used to check the conservation_id and use the 
+     *      CONSERVATION_OPTIONS array to create a sring answer for the conservation level
+     *
+     * @return  String  The string being returned is based on the id number
+     */
     public function conservation() {
         // echo self::CONSERVATION_OPTIONS[$this->conservation_id];
         if( $this->conservation_id > 0 ) {
--- private/db_connections.php
+++ private/db_connections.php
@@ -2,7 +2,7 @@
 
 function db_connect() {
     try {
-        $connection = new PDO('mysql:host='.DB_SERVER.'; dbname='.DB_NAME, DB_USER, DB_PASS); 
+        $connection = new PDO('mysql:host='.DB_SERVER.'; dbname='.DB_NAME. ';', DB_USER, DB_PASS); 
         return $connection;
     }
     
